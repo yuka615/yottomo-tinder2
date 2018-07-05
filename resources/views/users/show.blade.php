d7ecf608205baf622f78158189d9e94b020a81c9
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-07-05 06:35:15 +0000

    others

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,7 @@
         <aside class="col-xs-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">{{ $user->name }}</h3>
+                    <h3 class="panel-title">{{ $user->name }} ({{ $user->hometeam }},{{ $user->codingteam }})</h3>
                 </div>
                 <div class="panel-body">
                 <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->name, 500) }}" alt="">
@@ -14,6 +14,7 @@
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
+                <li><a href="#">MyProfile</a></li>
                 <li><a href="#">Like</a></li>
                 <li><a href="#">Future</a></li>
                 <li><a href="#">ズッ友</a></li>
